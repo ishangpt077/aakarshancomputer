8f09ce17b09baf971f6807e61c243cd2762d96f0
Ishan Gupta <130547668+ishangpt077@users.noreply.github.com>
2024-10-03 17:13:53 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// scripts.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.querySelector(".btn");
+
+    btn.addEventListener("click", () => {
+        alert("Exploring our services!");
+    });
+});
